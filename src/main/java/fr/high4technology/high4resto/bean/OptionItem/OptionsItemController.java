117f97e84cae64c771bc14a9b68fb9dc8c23b70a
Lemasson Patrick <l.patrick@high4technology.fr>
2020-11-12 11:21:06 +0100

    Correction du chemin

--- src/main/java/fr/high4technology/high4resto/bean/OptionItem/OptionsItemController.java
+++ src/main/java/fr/high4technology/high4resto/bean/OptionItem/OptionsItemController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/optionsItem")
+@RequestMapping("/api/optionsItem")
 @RequiredArgsConstructor
 public class OptionsItemController {
     @Autowired
