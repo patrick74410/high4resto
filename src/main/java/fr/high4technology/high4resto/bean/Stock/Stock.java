30bbb6cfb28263d2d517fece243417240b568d3d
Lemasson Patrick <l.patrick@high4technology.fr>
2020-11-23 20:53:40 +0100

    Changement de la structure de stock

--- src/main/java/fr/high4technology/high4resto/bean/Stock/Stock.java
+++ src/main/java/fr/high4technology/high4resto/bean/Stock/Stock.java
@@ -9,7 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import fr.high4technology.high4resto.bean.Struct.Disponibility;
+import fr.high4technology.high4resto.bean.Horaire.Horaire;
 import fr.high4technology.high4resto.bean.ItemCarte.ItemCarte;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,7 +27,7 @@ public class Stock {
     private ItemCarte item;
     @Getter
     @Builder.Default
-    private Disponibility disponibility=new Disponibility();
+    private Horaire disponibility=new Horaire();
     @Getter
     @Builder.Default
     private String inside=new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris")).getTime());
